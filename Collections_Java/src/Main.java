0024665c625e68ca6e3255fdc6778ef9f89f2466
Abdou Aziz Ba [67004] <abdouaziz.ba@orange-sonatel.com>
2023-02-06 16:19:05 +0000

    commit

--- Collections_Java/src/Main.java
+++ Collections_Java/src/Main.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*System.out.println("Hello world!");
 
         System.out.println("Test a ArrayList");
         ArrayList<String> people = new ArrayList<>();
@@ -31,6 +32,44 @@ public class Main {
         if (people.isEmpty()) {
             System.out.println("The list is empty!");
         }
-        System.out.println("The List printed is ..." + people);
+        System.out.println("The List printed is ..." + people);*/
+
+        //Test avec les LinkedList
+        System.out.println("Test a LinkedList");
+        //Declaration d'un linkedList de type Integer
+        LinkedList<Integer> list = new LinkedList<>();//Se trouve dans Java.util.linkedList
+        //Populate the LinkedList with add function linkedList
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        //impresion de la liste
+        System.out.println("The List printed is ..." + list);
+        //supprimer des éléments de la liste
+        list.remove(1);//suppression par index
+        System.out.println("The List printed is now ..." + list);
+        list.remove(Integer.valueOf(3)); //suppression par valeur
+        System.out.println("The List printed is now ..." + list);
+        //Effacer la liste
+        list.clear();
+        System.out.println("The List printed is now ..." + list);
+        //Populate list with offer function
+        list.offer(1);
+        list.offer(2);
+        list.offer(3);
+        //System.out.println("The List printed with offer function is  ..." + list);
+        //Order list by FIFO
+        //System.out.println("The List printed with offer function and FIFO order is  ..." + list.poll());
+        //Print the list on FIFO order with a poll function
+        while (!list.isEmpty()) {
+            System.out.println("The List printed with offer function and FIFO order is  ..." + list.poll());
+        }
+        //Print the list on LIFO order with a push and pop function
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        while (!list.isEmpty()) {
+            System.out.println("The List printed with offer function and LIFO order is  ..." + list.pop());
+        }
     }
 }
